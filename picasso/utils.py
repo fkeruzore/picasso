99e3a0750ae89f8abd9854984b596edcbfc8fc75
Florian Keruzore <fkeruzore@anl.gov>
2023-09-19 14:40:04 -0500

    add utils module with azimuthal profile routine

---
+++ picasso/utils.py
@@ -0,0 +1,38 @@
+from jax import jit, numpy as jnp
+
+
+@jit
+def azimuthal_profile(q, r, r_bins):
+    """Azimuthal profile of `q`, evaluated at radii `r`, with radial
+    binning `r_bins`.
+
+    Parameters
+    ----------
+    q : array
+        Quantity of interest
+    r : array
+        Radii at which the quantity of interest is evaluated
+    r_bins : array
+        Radial bins to be used for the radial profile
+
+    Returns
+    -------
+    array
+        center of 1d bins, shape=(n_bins - 1)
+    array
+        mean value of q in bins, shape=(n_bins - 1)
+    array
+        standard deviation of q in bins, shape=(n_bins - 1)
+    """
+    q, r = q.flatten(), r.flatten()
+
+    mean = []
+    std = []
+    for i_r in range(len(r_bins) - 1):
+        r_l, r_u = r_bins[i_r], r_bins[i_r + 1]
+        q_i = jnp.where((r >= r_l) & (r < r_u), q, jnp.nan)
+        mean.append(jnp.nanmean(q_i))
+        std.append(jnp.nanstd(q_i))
+
+    r_1d = r_bins[:-1] + 0.5 * jnp.ediff1d(r_bins)
+    return r_1d, jnp.array(mean), jnp.array(std)
