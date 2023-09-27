44aba0bed4667e49784b5d4ec167dace4d9d9de4
Florian Keruzore <fkeruzore@anl.gov>
2023-09-27 13:24:05 -0500

    add support for choosing stats in azimuthal profile

--- picasso/utils.py
+++ picasso/utils.py
@@ -1,10 +1,11 @@
 from jax import jit, Array
 import jax.numpy as jnp
+from functools import partial
 
 
-@jit
+@partial(jit, static_argnames=["stats"])
 def azimuthal_profile(
-    q: Array, r: Array, r_bins: Array
+    q: Array, r: Array, r_bins: Array, stats=(jnp.nanmean, jnp.nanstd)
 ) -> (Array, Array, Array):
     """Azimuthal profile of `q`, evaluated at radii `r`, with radial
     binning `r_bins`.
@@ -17,6 +18,9 @@ def azimuthal_profile(
         Radii at which the quantity of interest is evaluated
     r_bins : array
         Radial bins to be used for the radial profile
+    stat : str
+        Which statistic to be used within annuli, "mean" or "median",
+        defaults to "mean"
 
     Returns
     -------
@@ -31,11 +35,12 @@ def azimuthal_profile(
 
     mean = []
     std = []
+
     for i_r in range(len(r_bins) - 1):
         r_l, r_u = r_bins[i_r], r_bins[i_r + 1]
         q_i = jnp.where((r >= r_l) & (r < r_u), q, jnp.nan)
-        mean.append(jnp.nanmean(q_i))
-        std.append(jnp.nanstd(q_i))
+        mean.append(stats[0](q_i))
+        std.append(stats[1](q_i))
 
     r_1d = r_bins[:-1] + 0.5 * jnp.ediff1d(r_bins)
     return r_1d, jnp.array(mean), jnp.array(std)
