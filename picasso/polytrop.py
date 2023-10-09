b8b8606723f1e4f8874cecd5742682a89fd80afb
Florian Keruzore <fkeruzore@anl.gov>
2023-10-09 16:48:34 -0500

    re-parametrize to add theta_0 as the potential prefactor

--- picasso/polytrop.py
+++ picasso/polytrop.py
@@ -2,7 +2,7 @@ from jax import Array
 import jax.numpy as jnp
 
 
-def theta(phi: Array, Gamma: float) -> Array:
+def theta(phi: Array, Gamma: float, theta_0: float) -> Array:
     """
     Re-parametrized polytropic variable.
 
@@ -12,18 +12,29 @@ def theta(phi: Array, Gamma: float) -> Array:
         Normalized isolated gravitational potential (see Notes)
     Gamma : float
         Gas polytropic index
+    theta_0 : float
+        Potential prefactor (see Notes)
 
     Returns
     -------
     array
         Polytropic variable for each phi
+
+    Notes
+    -----
+    The potential `phi` is to be normalized to be zero at the bottom of
+        the well, and positive everywhere else. This definition males it
+        equivalent to (phi - phi_0) in the Ostriker model.
+    theta_0 is the pre-factor to multiply this potential with. In the
+        Ostriker model, theta_0 = ((Gamma - 1) / Gamma) * (rho_0 / P_0).
     """
-    t = 1.0 - ((Gamma - 1.0) / Gamma) * phi
+    # t = 1.0 - ((Gamma - 1.0) / Gamma) * theta_0 * phi
+    t = 1.0 - (theta_0 * phi)
     t = jnp.where(t >= 0.0, t, 0.0)
     return t
 
 
-def P_g(phi: Array, P_0: float, Gamma: float) -> Array:
+def P_g(phi: Array, P_0: float, Gamma: float, theta_0: float) -> Array:
     """
     Polytropic gas pressure (total pressure, therm. + kin.).
 
@@ -35,17 +46,27 @@ def P_g(phi: Array, P_0: float, Gamma: float) -> Array:
         Central gas pressure (total pressure, therm. + kin.)
     Gamma : float
         Gas polytropic index
+    theta_0 : float
+        Potential prefactor (see Notes)
 
     Returns
     -------
     array
         Gas pressure (total pressure, therm. + kin.) for each phi
+
+    Notes
+    -----
+    The potential `phi` is to be normalized to be zero at the bottom of
+        the well, and positive everywhere else. This definition males it
+        equivalent to (phi - phi_0) in the Ostriker model.
+    theta_0 is the pre-factor to multiply this potential with. In the
+        Ostriker model, theta_0 = ((Gamma - 1) / Gamma) * (rho_0 / P_0).
     """
-    t = theta(phi, Gamma)
+    t = theta(phi, Gamma, theta_0)
     return P_0 * (t ** (Gamma / (Gamma - 1.0)))
 
 
-def rho_g(phi: Array, rho_0: float, Gamma: float) -> Array:
+def rho_g(phi: Array, rho_0: float, Gamma: float, theta_0: float) -> Array:
     """
     Polytropic gas density.
 
@@ -57,18 +78,28 @@ def rho_g(phi: Array, rho_0: float, Gamma: float) -> Array:
         Central gas density
     Gamma : float
         Gas polytropic index
+    theta_0 : float
+        Potential prefactor (see Notes)
 
     Returns
     -------
     array
         Gas density for each phi
+
+    Notes
+    -----
+    The potential `phi` is to be normalized to be zero at the bottom of
+        the well, and positive everywhere else. This definition males it
+        equivalent to (phi - phi_0) in the Ostriker model.
+    theta_0 is the pre-factor to multiply this potential with. In the
+        Ostriker model, theta_0 = ((Gamma - 1) / Gamma) * (rho_0 / P_0).
     """
-    t = theta(phi, Gamma)
+    t = theta(phi, Gamma, theta_0)
     return rho_0 * (t ** (1.0 / (Gamma - 1.0)))
 
 
 def rho_P_g(
-    phi: Array, rho_0: float, P_0: float, Gamma: float
+    phi: Array, rho_0: float, P_0: float, Gamma: float, theta_0: float
 ) -> (Array, Array):
     """
     Polytropic gas density and pressure (total pressure, therm. + kin.).
@@ -83,6 +114,16 @@ def rho_P_g(
         Central gas pressure (total pressure, therm. + kin.)
     Gamma : float
         Gas polytropic index
+    theta_0 : float
+        Potential prefactor (see Notes)
+
+    Notes
+    -----
+    The potential `phi` is to be normalized to be zero at the bottom of
+        the well, and positive everywhere else. This definition males it
+        equivalent to (phi - phi_0) in the Ostriker model.
+    theta_0 is the pre-factor to multiply this potential with. In the
+        Ostriker model, theta_0 = ((Gamma - 1) / Gamma) * (rho_0 / P_0).
 
     Returns
     -------
@@ -91,7 +132,7 @@ def rho_P_g(
     array-like
         Gas pressure (total pressure, therm. + kin.) for each phi
     """
-    t = theta(phi, Gamma)
+    t = theta(phi, Gamma, theta_0)
     rho = rho_0 * (t ** (1.0 / (Gamma - 1.0)))
     P = P_0 * (t ** (Gamma / (Gamma - 1.0)))
     return rho, P
