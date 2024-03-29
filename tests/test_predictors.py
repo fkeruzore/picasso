a892342cd12d0adf6ec24fd85298b542fe4735e6
Florian Keruzore <fkeruzore@anl.gov>
2024-03-29 16:56:46 -0500

    add unit test for shapes in predictors.PicassoPredictor

--- tests/test_predictors.py
+++ tests/test_predictors.py
@@ -1,6 +1,6 @@
 import jax
 import jax.numpy as jnp
-from picasso.predictors import FlaxRegMLP
+from picasso import predictors
 
 
 def test_flax_reg_mlp():
@@ -9,7 +9,7 @@ def test_flax_reg_mlp():
     Y_DIM = 5
 
     # Create an instance of FlaxRegMLP
-    mlp = FlaxRegMLP(
+    mlp = predictors.FlaxRegMLP(
         X_DIM=X_DIM,
         Y_DIM=Y_DIM,
         hidden_features=(16, 16),
@@ -31,3 +31,44 @@ def test_flax_reg_mlp():
 
     # Check output values are within valid range
     assert jnp.all(y >= 0.0) and jnp.all(y <= 1.0)
+
+
+def test_picasso_predictor():
+    # Number of (halos, points) for which to predict
+    n_halos = 2
+    n_pts = 5
+
+    # Define input and output dimensions
+    X_DIM = 10
+    Y_DIM = 7
+
+    # Create an instance of FlaxRegMLP
+    mlp = predictors.FlaxRegMLP(
+        X_DIM=X_DIM,
+        Y_DIM=Y_DIM,
+        hidden_features=(16, 16),
+        activations=["selu", "selu", "selu", "soft_clip"],
+        extra_args_output_activation=[jnp.zeros(Y_DIM), jnp.ones(Y_DIM)],
+    )
+
+    # Generate random input
+    rng = jax.random.PRNGKey(0)
+    x = jax.random.normal(rng, (n_halos, X_DIM))
+
+    default_net_par = mlp.init(rng, x[0])
+
+    # Create an instance of PicassoPredictor
+    predictor = predictors.PicassoPredictor(mlp, default_net_par)
+
+    # Test predict_model_parameters method
+    y_pred = predictor.predict_model_parameters(x)
+    assert y_pred.shape == (n_halos, Y_DIM)
+
+    # Test predict_gas_model method
+    phi = jnp.ones((n_halos, n_pts))
+    r_R500 = jnp.ones((n_halos, n_pts))
+    gas_model = predictor.predict_gas_model(x, phi, r_R500)
+    assert len(gas_model) == 3
+    assert gas_model[0].shape == (n_halos, n_pts)
+    assert gas_model[1].shape == (n_halos, n_pts)
+    assert gas_model[2].shape == (n_halos, n_pts)
