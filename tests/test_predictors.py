e0e70e553f835e79becb327cd5c606847f6e624b
Florian Keruzore <fkeruzore@anl.gov>
2024-04-03 15:48:17 -0500

    Add tests and benchmarks for predicting gas properties and model parameters using pretrained model

--- tests/test_predictors.py
+++ tests/test_predictors.py
@@ -1,6 +1,14 @@
 import jax
 import jax.numpy as jnp
 from picasso import predictors
+import pickle
+import os
+import pytest
+
+here = os.path.dirname(os.path.abspath(__file__))
+path = f"{here}/data"
+with open(f"{path}/test_data_predictor.pkl", "rb") as f:
+    data_predictor = pickle.load(f)
 
 
 def test_flax_reg_mlp():
@@ -72,3 +80,44 @@ def test_picasso_predictor():
     assert gas_model[0].shape == (n_halos, n_pts)
     assert gas_model[1].shape == (n_halos, n_pts)
     assert gas_model[2].shape == (n_halos, n_pts)
+
+
+@pytest.mark.parametrize("jit", [True, False])
+def test_predict_model_params_pretrained(jit, benchmark):
+
+    predict_func = predictors.net1.predict_model_parameters
+    if jit:  # jit the function and call it once to compile
+        predict_func = jax.jit(predict_func)
+        _ = predict_func(data_predictor["x"][0])
+
+    # benchmark function (jitted or not)
+    y_pred = benchmark(predict_func, data_predictor["x"])
+    assert y_pred.shape == (data_predictor["x"].shape[0], 7)
+    assert jnp.all(jnp.isfinite(y_pred))
+
+
+@pytest.mark.parametrize("jit", [True, False])
+def test_predict_gas_properties_pretrained(jit, benchmark):
+    predict_func = predictors.net1.predict_gas_model
+    if jit:  # jit the function and call it once to compile
+        predict_func = jax.jit(predict_func)
+        _ = predict_func(
+            data_predictor["x"][0],
+            data_predictor["phi"][0],
+            data_predictor["r_R500"][0],
+        )
+
+    # benchmark function (jitted or not)
+    profs_pred = benchmark(
+        predict_func,
+        data_predictor["x"],
+        data_predictor["phi"],
+        data_predictor["r_R500"],
+    )
+    profs_pred = jnp.array(profs_pred)
+    assert profs_pred.shape == (
+        3,
+        data_predictor["x"].shape[0],
+        data_predictor["phi"].shape[1],
+    )
+    assert jnp.all(jnp.isfinite(profs_pred))
