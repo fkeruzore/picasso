7e73873175ed82d49bda5adb8b7b369f99bd7e66
Florian Keruzore <fkeruzore@anl.gov>
2024-04-17 14:16:19 -0500

    Remove utils/sph.py module

--- picasso/utils/__init__.py
+++ picasso/utils/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "Florian Kéruzoré"
 __email__ = "fkeruzore@anl.gov"
 
-__all__ = ["fitting", "nfw", "cic", "sph", "azimuthal_profile"]
+__all__ = ["fitting", "nfw", "cic", "azimuthal_profile"]
 
-from . import fitting, nfw, cic, sph
+from . import fitting, nfw, cic
 from .azimuthal_profile import azimuthal_profile
