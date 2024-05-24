e79117ac164bde392ddf0da2471cfae991aa7bea
Florian Keruzore <fkeruzore@anl.gov>
2024-05-24 16:05:28 -0500

    comment out obsolete tests

--- tests/test_hacc_helper.py
+++ tests/test_hacc_helper.py
@@ -1,9 +1,8 @@
 import numpy as np
-import jax.numpy as jnp
 import h5py
 import os
 from astropy.cosmology import FlatLambdaCDM
-from picasso.data_helpers import hacc, fitters
+from picasso.data_helpers import hacc
 import pytest
 
 cosmo = FlatLambdaCDM(67.66, 0.30964, Ob0=0.04897)
@@ -50,47 +49,48 @@ def test_hacc_cutouts_profiles(z):
         )
 
 
-@pytest.mark.parametrize(
-    ["case", "z"],
-    [
-        ("from profiles", 0.0),
-        ("from particles", 0.0),
-        ("from profiles", 0.5),
-        ("from particles", 0.5),
-    ],
-)
-def test_fit_hacc_profiles(case, z):
-    if z == 0.0:
-        snap = "624"
-    elif z == 0.5:
-        snap = "498"
-    cutout_go = hacc.HACCCutout(
-        d[snap]["h"], d[snap]["parts_go"], z, 576.0, cosmo, is_hydro=False
-    )
-    if case == "from profiles":
-        profs_hy = hacc.HACCSODProfiles.from_sodpropertybins(
-            d[snap]["h_ad"], d[snap]["profs_ad"], z, cosmo, is_hydro=True
-        )
-        profs_hy.rebin(jnp.logspace(-1.25, 0.25, 8))
-    elif case == "from particles":
-        cutout_hy = hacc.HACCCutout(
-            d[snap]["h_ad"],
-            d[snap]["parts_ad"],
-            z,
-            576.0,
-            cosmo,
-            is_hydro=True,
-        )
-        profs_hy = hacc.HACCSODProfiles.from_cutout(
-            cutout_hy, jnp.logspace(-1.25, 0.25, 8)
-        )
-
-    res_pol = fitters.fit_gas_profiles(
-        cutout_go,
-        profs_hy,
-        which_P="tot",
-        fit_fnt=False,
-        return_history=True,
-        n_steps=5_000,
-    )
-    assert res_pol.bl < 1.0, f"Large loss value: {res_pol.bl:.2e}"
+# @pytest.mark.parametrize(
+#     ["case", "z"],
+#     [
+#         ("from profiles", 0.0),
+#         ("from particles", 0.0),
+#         ("from profiles", 0.5),
+#         ("from particles", 0.5),
+#     ],
+# )
+# def test_fit_hacc_profiles(case, z):
+#     if z == 0.0:
+#         snap = "624"
+#     elif z == 0.5:
+#         snap = "498"
+#     cutout_go = hacc.HACCCutout(
+#         d[snap]["h"], d[snap]["parts_go"], z, 576.0, cosmo, is_hydro=False
+#     )
+#     if case == "from profiles":
+#         profs_hy = hacc.HACCSODProfiles.from_sodpropertybins(
+#             d[snap]["h_ad"], d[snap]["profs_ad"], z, cosmo, is_hydro=True
+#         )
+#         profs_hy.rebin(jnp.logspace(-1.25, 0.25, 8))
+#     elif case == "from particles":
+#         cutout_hy = hacc.HACCCutout(
+#             d[snap]["h_ad"],
+#             d[snap]["parts_ad"],
+#             z,
+#             576.0,
+#             cosmo,
+#             is_hydro=True,
+#         )
+#         profs_hy = hacc.HACCSODProfiles.from_cutout(
+#             cutout_hy, jnp.logspace(-1.25, 0.25, 8)
+#         )
+#
+#     res_pol = fitters.fit_gas_profiles(
+#         cutout_go,
+#         profs_hy,
+#         which_P="tot",
+#         fit_fnt=False,
+#         return_history=True,
+#         n_steps=5_000,
+#     )
+#     assert res_pol.bl < 1.0, f"Large loss value: {res_pol.bl:.2e}"
+#
